a2f0d8ea86506ac5f79c1d5c42cf97cddfc03721
Eldho Reji <eldhoreji15@gmail.com>
2024-01-29 01:42:54 +0530

    account-management

--- eokart/customers/views.py
+++ eokart/customers/views.py
@@ -1,37 +1,64 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .models import Customer
 
 # Create your views here.
 
 def show_account(request):
-
+  
+  context={}
   if request.POST and 'signup_button' in request.POST:
-    try:    
+    context['signup_button']=True
+      
+    try:
+         username=request.POST.get('username')
+         password=request.POST.get('password')
+         email=request.POST.get('email')
+         address=request.POST.get('address')
+         phone_number=request.POST.get('phonenumber')
 
-        username=request.POST.get('username')
-        password=request.POST.get('password')
-        email=request.POST.get('email')
-        address=request.POST.get('address')
-        phone_number=request.POST.get('phone_number')
 
-        #creates user account
-        user=User.objects.create(
-            username=username,
-            password=password,
-            email=email
-        )
-        #customer account
-        customer=Customer.objects.create(
-            user=username,
+        # Create the User object
+         user=User.objects.create_user(
+             username=username,
+             email=email,
+             password=password
+    )
+
+        # Create the associated Customer object
+         customer=Customer.objects.create(
+            user=user,
             address=address,
             phone=phone_number
-        )
-        return redirect('home') 
+    )
+         sucess_message="User Registered Successfully"
+         messages.success(request,sucess_message)
+        
     except Exception as e:
-          error_message="Username Already Taken!"
-          messages.error(request,error_message)
+       error_message="Username Already Taken"
+       messages.error(request,error_message)
+
+  if request.POST and 'login' in request.POST:
+      context['signup_button']=False
+     
+      username=request.POST.get('username')
+      password=request.POST.get('password')
+     
+      user=authenticate(username=username,password=password)
+      if user: 
+         login(request,user)
+         return redirect('home')
+      else:
+         messages.error(request,'Invalid User')
+         
+   
+  return render(request,'account.html',context)
 
 
-  return render(request,'account.html')
+def sign_out(request):
+    logout(request)
+    return redirect(request,'home')
+
+
